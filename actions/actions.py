d9f0b4b263b538245baa9a51629151942cbe9456
PranavGupta73 <sg85453@gmail.com>
2022-06-27 13:41:33 +0530

    first commit

---
+++ actions/actions.py
@@ -0,0 +1,33 @@
+from dis import dis
+import string
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import csv
+import re
+
+class ActionReadCSV(Action):
+    def name(self) -> Text:
+         return "action_csv"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        id = tracker.get_slot('claim_id')
+        with open('claimstatus.csv','r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[1]==id:
+                    dispatcher.utter_message("Your claim is "+row[2]+"\n")
+                    if(row[2]=="Approved"):
+                        dispatcher.utter_message("Your claim amount is "+row[3]+"\n")
+                        return[]
+                    elif(row[2]=="Pending"):
+                        dispatcher.utter_message("We ask that you check again in a few days\n")
+                        return[]
+                    elif(row[2]=="Rejected"):
+                        dispatcher.utter_message("We are sorry but your damages are not covered under the policy that you purchased.\n")
+                        return[]
+        dispatcher.utter_message("Your entered name or id is incorrect, please type again.\n")
+        return[]
+                        
